d0d7f2eaca13d599f51693dcc774005eb98e22e6
Ronny Quispe <rqronny@gmail.com>
2023-02-22 21:34:02 -0500

    Reporte 1

---
+++ src/test/java/support/util.java
@@ -0,0 +1,50 @@
+package support;
+
+import definitions.hooks;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+import java.util.Set;
+
+public class util extends hooks {
+    public static WebDriverWait wait;
+    public static JavascriptExecutor js;
+
+    public util() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        js = (JavascriptExecutor) driver;
+    }
+
+    public void ventanaActiva(){
+        Set<String> identificadores = driver.getWindowHandles();
+        for (String identificador:identificadores){
+            driver.switchTo().window(identificador);
+        }
+    }
+
+    public void ventanaInicial(){
+        driver.close();
+        driver.switchTo().window("");
+    }
+    public void scrollVertical(){
+        js.executeScript("window.scrollBy(0,1000)");
+    }
+    public static void evidencias() throws IOException {
+        Date fecha = new Date();
+        SimpleDateFormat formato =  new SimpleDateFormat("ddMMyyyy_HHmmssSSS");
+        String path = "\\Users\\eloye\\IdeaProjects\\proyectoFebrero\\target\\evidencias\\";
+        String nombre = formato.format(fecha)+"_evidencia.jpg";
+
+        File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File(path+nombre));
+
+    }
+}
